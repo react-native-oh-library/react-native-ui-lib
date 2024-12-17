dfe41257d0d8dca71ef5cce8870374e9fe11a9e6
ZhuXionghui <n037406@archermind.com>
2024-12-17 18:19:58 +0800

    feat:harmony platform adapter

--- harmony/ui_lib/ts.ts
+++ harmony/ui_lib/ts.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
 export * from "./src/main/ets/UiLibPackage"
 export * from "./src/main/ets/KeyboardTrackingViewTempManagerTurboModule"
 export * from "./src/main/ets/KeyboardTrackingViewTempTurboModule"
